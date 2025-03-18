99b37cc778ad91b5112ffdbe1cf28e0ff7399cda
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2025-03-18 20:53:22 +0000

    classifier: euclidean + mahalanobis MDC

--- src/main.py
+++ src/main.py
@@ -6,7 +6,10 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from preprocessing.feature_extraction import BagOfWords
-from classifiers.minimum_distance_classifier import MinimumDistanceClassifier
+from classifiers.minimum_distance_classifier import (
+    EuclideanMinimumDistanceClassifier,
+    MahalanobisMinimumDistanceClassifier
+)
 from sklearn.metrics import accuracy_score
 
 
@@ -23,8 +26,10 @@ def main():
     test_data, train_data = train_test_split(dataset, test_size=0.8, random_state=42)
     train_data_labels = train_data['label']
     train_data.drop(columns=["FILENAME", "URL", "Domain", "TLD", "Title", "label"], inplace=True)
+    train_data = train_data.to_numpy()
     test_data_labels = test_data['label']
     test_data.drop(columns=["FILENAME", "URL", "Domain", "TLD", "Title", "label"], inplace=True)
+    test_data = test_data.to_numpy()
 
     '''
     # remove non numeric features TODO represent these features as numeric (BoW maybe)
@@ -41,20 +46,22 @@ def main():
     X_train_lda = lda.fit_transform(train_data, train_data_labels)
     X_test_lda = lda.transform(test_data)
 
+    '''
     plt.figure(figsize=(8, 6))
     sns.histplot(x=X_train_lda.flatten(), hue=train_data_labels, palette="viridis", kde=True, element="step")
     plt.title("LDA Transformation (1 Component)")
     plt.xlabel("LDA Component")
     plt.legend(title="Class")
     plt.show()
+    '''
 
-    mdc_raw = MinimumDistanceClassifier()
+    mdc_raw = MahalanobisMinimumDistanceClassifier()
     mdc_raw.fit(train_data, train_data_labels)
     predictions = mdc_raw.predict(test_data)
     accuracy = accuracy_score(predictions, test_data_labels)
     print("Accuracy without data preprocessing:", accuracy)
 
-    mdc_lda = MinimumDistanceClassifier()
+    mdc_lda = MahalanobisMinimumDistanceClassifier()
     mdc_lda.fit(X_train_lda, train_data_labels)
     predictions = mdc_lda.predict(X_test_lda)
     accuracy = accuracy_score(predictions, test_data_labels)
